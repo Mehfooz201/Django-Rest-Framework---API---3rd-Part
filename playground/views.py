9d000dc3b8f779dce8dded12db5b31d6322e3d9e
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-10-23 01:43:03 +0500

    mail_sending_done

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage, BadHeaderError
+from templated_mail.mail import BaseEmailMessage
+
 
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/mail.html',
+            context = {
+                'name' : 'Mehfooz',
+            }
+        )
+        message.send(['mehfooz.connect@gmail.com'])
+
+    except BadHeaderError:
+        pass
+
     return render(request, 'hello.html', {'name': 'Mosh'})
+
+
