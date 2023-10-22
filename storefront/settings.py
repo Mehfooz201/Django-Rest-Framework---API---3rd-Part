fea0b91f3591025d66a6aca39c1409fea4e0ab99
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-10-22 21:59:33 +0500

    done with image upload api

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
+
     'rest_framework',
     'djoser',
     'playground',
@@ -47,9 +49,12 @@ INSTALLED_APPS = [
     'tags',
     'likes',
     'core',
+
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -65,6 +70,12 @@ INTERNAL_IPS = [
     '127.0.0.1',
     # ...
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
+]
+
 
 ROOT_URLCONF = 'storefront.urls'
 
