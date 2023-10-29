1e2ef971b31314b83aab9439bf9fc09766360ac1
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-10-30 02:31:57 +0500

    done with cache

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 from random import randint
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1,5)
+    wait_time = between(1,6)
 
     @task(2)
     def view_products(self):
@@ -21,6 +21,11 @@ class WebsiteUser(HttpUser):
                          name='/store/carts/items',
                          json={'product_id' : product_id, 'quantity':1})
     
+
+    @task(3)
+    def say_hello(self):
+        self.client.get('playground/hello/')
+
     def on_start(self):
         response = self.client.post('/store/carts/')
         result = response.json()
--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render
-from django.core.mail import EmailMessage, BadHeaderError
-from templated_mail.mail import BaseEmailMessage
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+import requests
 
-from .task import notify_customers
+# from django.core.mail import EmailMessage, BadHeaderError
+# from templated_mail.mail import BaseEmailMessage
+# from .task import notify_customers
 
+@cache_page(5*60)
 def say_hello(request):
-    notify_customers.delay('Hello')
-    # try:
-    #     message = BaseEmailMessage(
-    #         template_name='emails/mail.html',
-    #         context = {
-    #             'name' : 'Mehfooz',
-    #         }
-    #     )
-    #     message.send(['mehfooz.connect@gmail.com'])
+    response = requests.get('http://httpbin.org/delay/2')
+    data = response.json()
 
-    # except BadHeaderError:
-    #     pass
-
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': data})
 
 
+
+
+
+
+# def say_hello(request):
+#     notify_customers.delay('Hello')
+#     # try:
+#     #     message = BaseEmailMessage(
+#     #         template_name='emails/mail.html',
+#     #         context = {
+#     #             'name' : 'Mehfooz',
+#     #         }
+#     #     )
+#     #     message.send(['mehfooz.connect@gmail.com'])
+
+#     # except BadHeaderError:
+#     #     pass
+
+#     return render(request, 'hello.html', {'name': 'Mosh'})
