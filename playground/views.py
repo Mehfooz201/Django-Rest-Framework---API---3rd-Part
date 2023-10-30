2ba2520109bb8d8ebe39ccab3b79ee7323a13d64
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-10-30 16:48:34 +0500

    production settings

--- playground/views.py
+++ playground/views.py
@@ -1,36 +1,20 @@
-from django.shortcuts import render
 from django.core.cache import cache
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import logging
 import requests
 
-# from django.core.mail import EmailMessage, BadHeaderError
-# from templated_mail.mail import BaseEmailMessage
-# from .task import notify_customers
+logger = logging.getLogger(__name__) 
 
-@cache_page(5*60)
-def say_hello(request):
-    response = requests.get('http://httpbin.org/delay/2')
-    data = response.json()
-
-    return render(request, 'hello.html', {'name': data})
-
-
-
-
-
-
-# def say_hello(request):
-#     notify_customers.delay('Hello')
-#     # try:
-#     #     message = BaseEmailMessage(
-#     #         template_name='emails/mail.html',
-#     #         context = {
-#     #             'name' : 'Mehfooz',
-#     #         }
-#     #     )
-#     #     message.send(['mehfooz.connect@gmail.com'])
-
-#     # except BadHeaderError:
-#     #     pass
-
-#     return render(request, 'hello.html', {'name': 'Mosh'})
+class HelloView(APIView):
+    def get(self, request):
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
+        return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,9 +159,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
@@ -217,6 +218,35 @@ CACHES = {
         "TIMEOUT" : 10 * 60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': os.environ.get('DJANGO_LOG_LEVEL', 'INFO')
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} ({levelname}) - {name} - {message}',
+            'style': '{' 
         }
     }
 }
